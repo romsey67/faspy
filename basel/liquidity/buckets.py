547d3207ceecc6607a9eb04cd1e49a03af54753f
Nur Liyana Hayati Rosli <rosliyana96@icloud.com>
2020-11-16 12:17:40 +0800

    def bucketing - testing

--- basel/liquidity/buckets.py
+++ basel/liquidity/buckets.py
@@ -1,9 +1,11 @@
 import numba
 from numpy import datetime64 as dt64
-from faspy.interestrate.rmp_dates import day_count_factor as day_cf
+from .rmp_dates import day_count_factor as day_cf
+from faspy.interestrate.rmp_curves import interpolation
+import math
 
 
-def bucketing(data, buckets, value_date, df=None, df_shifted=None):
+def bucketing(data, buckets, value_date, dfcurve=None, dfcurve_shifted=None):
     """
     Generate bonds coupon structures.
 
@@ -17,11 +19,49 @@ def bucketing(data, buckets, value_date, df=None, df_shifted=None):
                 days, df, rate
     """
     vdate = dt64(value_date,"D")
-    new_data = [x.update({"days": _datediff(vdate, dt64(x["date"],"D")), "tenor": day_cf("Actual/365", vdate, dt64(x["date"],"D"))}) for x in data]
+    mydata = list(data)
+    calc_data = [{"days": _datediff(vdate, dt64(x["date"],"D")), "tenor": day_cf("Actual/365", vdate, dt64(x["date"],"D"))} for x in mydata]
 
+    df_xaxis = [x["times"] for x in dfcurve]
+    df_yaxis = [x["df"] for x in dfcurve]
+    i_func = interpolation(df_xaxis,df_yaxis,1)
+    df_array = [{"df": ifunc(x["tenor"])} for x in calc_data]
+
+    dtlen = len(mydata)
+    newdata=[]
+    for i in range(dtlen):
+        old = dict(mydata[i])
+        new = dict(calc_data[i])
+        df_dic = df_array[i]
+        old.update(new)
+        old.update(df_dic)
+        newdata.append(old)
     return new_data
 
+    mdur = [{"mod_duration": _mod_duration(float(x["amount"]),float(x["tenor"], float(x["df"])))} for x in new_data]
 
+    for i in range(dtlen)
+        old = new_data[i]
+        new = mdur[i]
+        old.update(new)
+
+    return new_data
+    
+
+@numba.njit('float64(float64, float64)')
+def _mod_duration(amount, time, df):
+    crate = -math.log(df)/time
+    value01 = amount * df
+    df01 = math.exp(-time * (crate + 0.0001))
+    value02 = amount * df01
+    return (value02-value01) / 0.0001
+
+
+
+@numba.njit('float64(float64, float64)')
+def _cal_crate(time, df):
+    crate = -math.log(df)/time
+    return float(crate)
 
 @numba.njit
 def _datediff(date1, date2):
